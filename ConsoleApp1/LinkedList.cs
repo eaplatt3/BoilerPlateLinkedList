5409f95aef25d4dd3e73467fef0b3b695f39774e
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2020-01-10 13:40:18 -0500

    Create Node Class

---
+++ ConsoleApp1/LinkedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class LinkedList
+    {
+        //The First node of the Linked List
+        //The Node will be an Object that is null by default
+        Node head;
+
+        class Node
+        {
+            int data;
+            Node nect;
+
+            //Constructor to Make a Node
+            Node(int d) { data = d; }
+        }
+    }
+}
